6cf8d70e47905cf752fb043870882f3298e8faa2
PedroLucas <pedromedeiros@ads.fiponline.edu.br>
2026-03-25 14:44:08 -0300

    atualizando

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv 
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,15 +78,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-'default': {
- 'ENGINE':
-'django.db.backends.postgresql',
- 'NAME': 'BancoDjango5',
- 'USER': 'postgres',
- 'PASSWORD': 'p09017816490s',
- 'HOST': 'localhost',
- 'PORT': '5432',
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 # Password validation
